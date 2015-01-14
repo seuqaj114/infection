d890cc30191e72568acf3b152f5bb7a06ba8fa4d
seuqaj114 <migjacques@hotmail.com>
2015-01-14 21:51:00 +0000

    Completed simple version of total and limited infection.

--- total_infection.py
+++ total_infection.py
@@ -2,6 +2,7 @@ from uuid import uuid4, UUID
 
 hierarchy={}
 standard_version=1.0
+count=0
 
 class User():
 	"""
@@ -10,6 +11,7 @@ class User():
 		-coach (single id; max_lenght=1; set to None if in topmost layer)
 		-students (list of id's; max_lenght=unlimited)
 		-name (string)
+		-student_count (integer) -> number of students in lower graph + self
 	"""
 
 	def __init__(self,version,coach,students,name):
@@ -17,9 +19,11 @@ class User():
 		self.coach = coach
 		self.students = students
 		self.name = name
+		self.student_count = None
 
 	def __str__(self):
-		return "(version=%s, coach=%s, students=%s, name=%s)" % (self.version,self.coach,self.students,self.name)
+		return "(version=%s, coach=%s, students=%s, name=%s, student_cnt=%s)" % \
+				(self.version,self.coach,self.students,self.name,self.student_count)
 
 def recursive_add(coach_id,user_list):
 	global hierarchy
@@ -51,8 +55,6 @@ def recursive_add(coach_id,user_list):
 def spread_infection(user_id,version):
 	global hierarchy
 
-	if user_id == None:
-		return 0
 
 	#	Test if this user's version is already updated.
 	if hierarchy[user_id].version != version:
@@ -75,6 +77,39 @@ def spread_infection(user_id,version):
 
 	return 0
 
+def infect_below(user_id,version):
+	#	No need to check for version, since infection is done one-way
+
+	global hierarchy
+
+	hierarchy[user_id].version = version
+
+	if hierarchy[user_id].students != None:
+		for student_id in hierarchy[user_id].students:
+			infect_below(student_id,version)
+	else:
+		pass
+
+	return 0
+
+
+def spread_limited_infection(infect_count,version):
+	global hierarchy
+
+	cnt_list=[(user_id,hierarchy[user_id].student_count) for user_id in hierarchy.keys()]
+	print cnt_list
+	cnt_list.sort(key=lambda x: abs(x[1]-infect_count))
+
+	#	Set priority in case of a tie???
+
+	#	Propagation only happens to lower levels.
+	infect_below(cnt_list[0][0],version)
+
+	for key, value in hierarchy.items():
+		print key, value
+
+	return 0
+
 
 def set_hierarchy(user_list):
 
@@ -87,7 +122,36 @@ def set_hierarchy(user_list):
 	global hierarchy
 
 	recursive_add(None,user_list)
+	set_student_counts()
 
 	for key, value in hierarchy.items():
 		print key, value
+
+
+def recursive_count(user_id):
+	global count
+	global hierarchy
+
+	count+=1
+
+	if hierarchy[user_id].students != None:
+		for student_id in hierarchy[user_id].students:
+			recursive_count(student_id)
+	else:
+		pass
+
+	return 0
+
+def set_student_counts():
+	global hierarchy
+	global count
+
+	for user_id in hierarchy.keys():
+		count = 0
+		recursive_count(user_id)
+		hierarchy[user_id].student_count = count
+
+	print "Student counts set."
+
+
 
